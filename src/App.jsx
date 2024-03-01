7bb1b62d1a3541256e56896fb4218fa73071dc05
saifur2233 <saifurnstuiit223344@gmail.com>
2024-03-01 18:09:40 +0600

    Initial Configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <>
+      <div>
+         <Button variant="primary">Primary</Button>{' '}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
